881499a1f9bc915154c9f46adc9e8f2564e5b99a
germeier <germeier>
2004-04-23 10:27:30 +0000

    remove all files from CVS which can be automagically generated by the build environment

--- autogen.sh
+++ autogen.sh
@@ -5,8 +5,15 @@
 # Andreas Buesching  <crunchy@tzi.de>
 # $id$
 
+# minimum needed software versions:
+#
+# * libtoolize (GNU libtool) 1.4.3
+# * aclocal (GNU automake) 1.6.3
+# * autoconf (GNU Autoconf) 2.57
+# * automake (GNU automake) 1.6.3
+
 export WANT_AUTOMAKE="1.7"
 
-aclocal && autoconf && automake -a && ./configure $@
+libtoolize -c -f && aclocal && autoconf && automake -a -c -f && ./configure $@
 
 # end of autogen.sh 
